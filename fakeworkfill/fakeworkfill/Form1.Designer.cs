c0b0fed8fd271738c5479c315b1e2fba40abba35
Haz <master@hazonline.co.uk>
2018-03-21 23:53:13 +0000

    Add files via upload

---
+++ fakeworkfill/fakeworkfill/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace fakeworkfill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.Folder_path_tb = new System.Windows.Forms.TextBox();
+            this.browse_btn = new System.Windows.Forms.Button();
+            this.gn_f_btn = new System.Windows.Forms.Button();
+            this.gn_t_btn = new System.Windows.Forms.Button();
+            this.genfiles_btn = new System.Windows.Forms.Button();
+            this.nb_fs_tb = new System.Windows.Forms.TextBox();
+            this.close_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Folder_path_tb
+            // 
+            this.Folder_path_tb.Location = new System.Drawing.Point(0, 0);
+            this.Folder_path_tb.Name = "Folder_path_tb";
+            this.Folder_path_tb.Size = new System.Drawing.Size(150, 20);
+            this.Folder_path_tb.TabIndex = 0;
+            // 
+            // browse_btn
+            // 
+            this.browse_btn.Location = new System.Drawing.Point(150, 0);
+            this.browse_btn.Name = "browse_btn";
+            this.browse_btn.Size = new System.Drawing.Size(50, 20);
+            this.browse_btn.TabIndex = 1;
+            this.browse_btn.Text = "Browse";
+            this.browse_btn.UseVisualStyleBackColor = true;
+            this.browse_btn.Click += new System.EventHandler(this.browse_btn_Click);
+            // 
+            // gn_f_btn
+            // 
+            this.gn_f_btn.BackColor = System.Drawing.Color.Red;
+            this.gn_f_btn.FlatAppearance.BorderSize = 0;
+            this.gn_f_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.gn_f_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.gn_f_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gn_f_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gn_f_btn.ForeColor = System.Drawing.Color.White;
+            this.gn_f_btn.Location = new System.Drawing.Point(0, 20);
+            this.gn_f_btn.Name = "gn_f_btn";
+            this.gn_f_btn.Size = new System.Drawing.Size(75, 50);
+            this.gn_f_btn.TabIndex = 1;
+            this.gn_f_btn.Text = "Random names";
+            this.gn_f_btn.UseVisualStyleBackColor = false;
+            this.gn_f_btn.Click += new System.EventHandler(this.gn_f_btn_Click);
+            // 
+            // gn_t_btn
+            // 
+            this.gn_t_btn.BackColor = System.Drawing.Color.Lime;
+            this.gn_t_btn.FlatAppearance.BorderSize = 0;
+            this.gn_t_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.gn_t_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.gn_t_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gn_t_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gn_t_btn.ForeColor = System.Drawing.Color.White;
+            this.gn_t_btn.Location = new System.Drawing.Point(75, 20);
+            this.gn_t_btn.Name = "gn_t_btn";
+            this.gn_t_btn.Size = new System.Drawing.Size(75, 50);
+            this.gn_t_btn.TabIndex = 1;
+            this.gn_t_btn.Text = "Realistic names";
+            this.gn_t_btn.UseVisualStyleBackColor = false;
+            this.gn_t_btn.Click += new System.EventHandler(this.gn_t_btn_Click);
+            // 
+            // genfiles_btn
+            // 
+            this.genfiles_btn.BackColor = System.Drawing.Color.Lime;
+            this.genfiles_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.genfiles_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.genfiles_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.genfiles_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genfiles_btn.ForeColor = System.Drawing.Color.White;
+            this.genfiles_btn.Location = new System.Drawing.Point(0, 130);
+            this.genfiles_btn.Name = "genfiles_btn";
+            this.genfiles_btn.Size = new System.Drawing.Size(250, 75);
+            this.genfiles_btn.TabIndex = 1;
+            this.genfiles_btn.Text = "Create";
+            this.genfiles_btn.UseVisualStyleBackColor = false;
+            this.genfiles_btn.Click += new System.EventHandler(this.genfiles_btn_Click);
+            // 
+            // nb_fs_tb
+            // 
+            this.nb_fs_tb.Location = new System.Drawing.Point(0, 110);
+            this.nb_fs_tb.Name = "nb_fs_tb";
+            this.nb_fs_tb.Size = new System.Drawing.Size(150, 20);
+            this.nb_fs_tb.TabIndex = 0;
+            this.nb_fs_tb.Text = "1";
+            // 
+            // close_btn
+            // 
+            this.close_btn.FlatAppearance.BorderSize = 0;
+            this.close_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.close_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.close_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close_btn.Location = new System.Drawing.Point(230, 0);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(20, 20);
+            this.close_btn.TabIndex = 1;
+            this.close_btn.Text = "X";
+            this.close_btn.UseVisualStyleBackColor = true;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 90);
+            this.label1.Margin = new System.Windows.Forms.Padding(5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(250, 250);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gn_t_btn);
+            this.Controls.Add(this.gn_f_btn);
+            this.Controls.Add(this.genfiles_btn);
+            this.Controls.Add(this.close_btn);
+            this.Controls.Add(this.browse_btn);
+            this.Controls.Add(this.nb_fs_tb);
+            this.Controls.Add(this.Folder_path_tb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox Folder_path_tb;
+        private System.Windows.Forms.Button browse_btn;
+        private System.Windows.Forms.Button gn_f_btn;
+        private System.Windows.Forms.Button gn_t_btn;
+        private System.Windows.Forms.Button genfiles_btn;
+        private System.Windows.Forms.TextBox nb_fs_tb;
+        private System.Windows.Forms.Button close_btn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ fakeworkfill/fakeworkfill/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+
+namespace fakeworkfill
+{
+    public partial class Form1 : Form
+    {
+        public bool gn_b = true;
+        public string fd_path = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gn_t_btn_Click(object sender, EventArgs e)
+        {
+            gn_b = true;
+            gn_f_btn.BackColor = Color.Red;
+            gn_t_btn.BackColor = Color.Lime;
+        }
+
+        private void gn_f_btn_Click(object sender, EventArgs e)
+        {
+            gn_b = false;
+            gn_t_btn.BackColor = Color.Red;
+            gn_f_btn.BackColor = Color.Lime;
+        }
+
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void browse_btn_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (Directory.Exists(folderBrowserDialog1.SelectedPath)) 
+                    fd_path = folderBrowserDialog1.SelectedPath;
+                Folder_path_tb.Text = fd_path;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Folder_path_tb.Text = fd_path;
+        }
+        public ulong count = 1;
+        private void genfiles_btn_Click(object sender, EventArgs e)
+        {
+            if ((nb_fs_tb.Text != "") && (nb_fs_tb.Text != null)){
+                try
+                {
+                    count = Convert.ToUInt64(nb_fs_tb.Text);
+                    ulong x = 1;
+                    
+                    while (x <= count)
+                    {
+                        string tmp_n = (fd_path+"\\" + random_string(_rn.Next(3,15)) +".docx");
+                        if (!System.IO.File.Exists(tmp_n))
+                        {
+                            System.IO.StreamWriter sw = new System.IO.StreamWriter(tmp_n);
+                            
+                            sw.Write(random_string(_rn.Next(1000,70000)));
+                            sw.Close();
+                            x++;
+                        }
+                                                
+                        
+                        
+                    }
+                    
+                    MessageBox.Show(x-1+" files made of the "+count+" specified");
+                }
+                catch (Exception error)
+                {
+                    Clipboard.SetText( error.Message.ToString());
+                    if ((error.Message.ToString() == "Input string was not in a correct format.")||(error.Message.ToString() == "Value was either too large or too small for a UInt64."))
+                        MessageBox.Show("Number input is not a number between 0 and " + ulong.MaxValue, "Input error");
+                    else
+                        MessageBox.Show(error.Message.ToString(), "Error with prep");
+                }
+            }
+        }
+        Random _rn = new Random();
+        public string random_string(int ln)
+        {
+            
+            string temp = "";
+            int i = 0;
+            while (i < ln)
+            {
+                if(_rn.Next(0,2) == 1)
+                    temp += ((char)_rn.Next(65, 90));
+                else
+                    temp += ((char)_rn.Next(97, 122));
+                i++;
+            }
+            
+            return temp;
+        }
+    }
+}
